d0343aba69e6b252a0d1f791089a352fa6a7f2d8
Redin Sergey <redin.s.o@yandex.ru>
2016-07-14 10:37:39 +0300

    работа

---
+++ StyleguideGenerator/src/StyleguideGenerator/Models/Data/Section.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StyleguideGenerator.Models.Data
+{
+    public class Section
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImportantDesc { get; set; }
+
+        public Dictionary<string,string> Vars { get; set; }
+
+        public List<SubSection> ChildList { get; set; }
+    }
+
+    public class SubSection :Section
+    {
+        public List<VarExample> VarExamples { get; set; }
+    }
+
+    public class SectionExample
+    {
+        
+    }
+
+    public class VarExample
+    {
+        public string Value { get; set; }
+
+        public string Text { get; set; }
+
+        public string Example { get; set; }
+    }
+}
