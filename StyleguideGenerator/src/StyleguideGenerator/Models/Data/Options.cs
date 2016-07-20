f66a12c17e3bde3b0b562a58b44f3e7703fe4c0c
Redin Sergey <redin.s.o@yandex.ru>
2016-07-20 15:58:57 +0300

    модели файлов

---
+++ StyleguideGenerator/src/StyleguideGenerator/Models/Data/Options.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace StyleguideGenerator.Models.Data
+{
+    /// <summary>
+    /// Группа параметров раздела
+    /// </summary>
+    public class SectionOptionsGroup
+    {
+        /// <summary>
+        /// Список параметров раздела
+        /// </summary>
+        private List<SectionOption> Vars { get; set; }
+    }
+    /// <summary>
+    /// Параметр раздела
+    /// </summary>
+    public class SectionOption
+    {
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Значение
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ StyleguideGenerator/src/StyleguideGenerator/Models/Data/Sample.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace StyleguideGenerator.Models.Data
+{
+    /// <summary>
+    /// Примеры раздела
+    /// </summary>
+    public class SectionSamples
+    {
+        /// <summary>
+        /// HTML код для примера
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Отображение HTML кода
+        /// </summary>
+        public bool ShowCode { get; set; }
+        /// <summary>
+        /// Список примеров
+        /// </summary>
+        public List<Sample> Samples { get; set; }
+    }
+    /// <summary>
+    /// Пример в разделе
+    /// </summary>
+    public class Sample
+    {
+        /// <summary>
+        /// Модификатор для замены 
+        /// </summary>
+        public string Modifier { get; set; }
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Текс для отображения
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Тип
+        /// </summary>
+        public int Type { get; set; }
+    }
+}
