6d5dba9740dff97d59bc88bfc304fb241719d53c
Redin Sergey <redin.s.o@yandex.ru>
2016-07-21 08:57:09 +0300

    работа

--- StyleguideGenerator/src/StyleguideGenerator/Models/Data/Section.cs
+++ StyleguideGenerator/src/StyleguideGenerator/Models/Data/Section.cs
@@ -5,22 +5,38 @@ using System.Threading.Tasks;
 
 namespace StyleguideGenerator.Models.Data
 {
+    /// <summary>
+    /// Раздел 
+    /// </summary>
     public class Section
     {
+        /// <summary>
+        /// Заголовок
+        /// </summary>
         public string Title { get; set; }
-
+        /// <summary>
+        /// Описание
+        /// </summary>
         public string Description { get; set; }
-
+        /// <summary>
+        /// Выделенное описание
+        /// </summary>
         public string HighlightedDescription { get; set; }
-
+        /// <summary>
+        /// Дополнительное описание
+        /// </summary>
         public string SubDescription { get; set; }
-
-        public string ImportantDesc { get; set; }
-
+        /// <summary>
+        /// Группы параметров 
+        /// </summary>
         public List<SectionOptionsGroup> OptionsGroups { get; set; }
-        
+        /// <summary>
+        /// Примеры
+        /// </summary>
         public List<Sample> Samples { get; set; }  
-
+        /// <summary>
+        /// Подразделы
+        /// </summary>
         public List<Section> ChildList { get; set; }
     }
 }
--- StyleguideGenerator/src/StyleguideGenerator/Models/Data/SectionTree.cs
+++ StyleguideGenerator/src/StyleguideGenerator/Models/Data/SectionTree.cs
@@ -1,16 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace StyleguideGenerator.Models.Data
 {
+    /// <summary>
+    /// Список разделов
+    /// </summary>
     public class SectionTree
     {
+        /// <summary>
+        /// Заголовок
+        /// </summary>
         public string Title { get; set; }
-
-        public List<> 
+        /// <summary>
+        /// Элементы списка
+        /// </summary>
+        public List<SectionTreeEl> Els { get; set; }
     }
-
+    /// <summary>
+    /// Элемент списка разделов
+    /// </summary>
     public class SectionTreeEl
+    {
+        /// <summary>
+        /// Индекс
+        /// </summary>
+        public int Index { get; set; }
+        /// <summary>
+        /// Заголовок
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Индексная строка
+        /// </summary>
+        public string IndexString { get; set; }
+        /// <summary>
+        /// Подразделы
+        /// </summary>
+        public List<SectionTreeEl> SubEls { get; set; } 
+    }
 }
